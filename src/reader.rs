520c4b2425b32764e8b2967baab44e9ffe380aac
Milan Opath <milan.opath@gmail.com>
2017-07-20 09:31:17 +0200

    added ReaderBuilder + tests

--- src/reader.rs
+++ src/reader.rs
@@ -4,6 +4,45 @@ use super::csv_core;
 use std::io::{self, BufRead};
 use std::error::Error;
 
+pub struct ReaderBuilder {
+    buffer_cap: usize,
+    core_builder: Box<csv_core::ReaderBuilder>,
+}
+
+impl Default for ReaderBuilder {
+    fn default() -> Self {
+        ReaderBuilder {
+            buffer_cap: 8 * (1<<10),
+            core_builder: Box::new(csv_core::ReaderBuilder::default()),
+        }
+    }
+}
+
+impl ReaderBuilder {
+    pub fn delimiter(mut self, del: u8) -> Self {
+        self.core_builder.delimiter(del);
+        self
+    }
+    
+    pub fn terminator(mut self, term: csv_core::Terminator) -> Self {
+        self.core_builder.terminator(term);
+        self
+    }
+
+    pub fn buffer_capacity(mut self, cap: usize) -> Self {
+        self.buffer_cap = cap;
+        self
+    }
+
+    pub fn from_reader<R: io::Read>(&self, rdr: R) -> Reader<R> {
+        Reader {
+            core: Box::new(self.core_builder.build()),
+            rdr: io::BufReader::with_capacity(self.buffer_cap, rdr),
+            state: ReaderState { eof: false },
+        }
+    }
+}
+
 pub struct Reader<R> {
     core: Box<csv_core::Reader>,
     rdr: io::BufReader<R>,
@@ -15,7 +54,7 @@ struct ReaderState {
     eof: bool,
 }
 
-impl<R> Reader<R> where R: io::Read {
+impl<R: io::Read> Reader<R> {
     #[inline]
     pub fn read_record(
         &mut self, 
@@ -59,5 +98,34 @@ impl<R> Reader<R> where R: io::Read {
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ReaderBuilder;
+    use record::RecordBuilder;
+    
+    #[test]
+    fn read_1() {
+        let data = "1,Aragorn,The Lord of the Rings\n2,Jon Snow,The Song of Ice and Fire";
+        let mut rdr = ReaderBuilder::default().from_reader(data.as_bytes());
+        let mut rec = RecordBuilder::default().build().unwrap();
+
+        let _ = rdr.read_record(&mut rec).unwrap();
+
+        assert_eq!(rec.get_field(0), Some(&b"1"[..]));
+        assert_eq!(rec.get_field(1), Some(&b"Aragorn"[..]));
+        assert_eq!(rec.get_field(2), Some(&b"The Lord of the Rings"[..]));
+        assert_eq!(rec.get_field(3), None);
+        assert_eq!(rec.get_field(4), None);
+
+        let _ = rdr.read_record(&mut rec).unwrap();
+
+        assert_eq!(rec.get_field(0), Some(&b"2"[..]));
+        assert_eq!(rec.get_field(1), Some(&b"Jon Snow"[..]));
+        assert_eq!(rec.get_field(2), Some(&b"The Song of Ice and Fire"[..]));
+        assert_eq!(rec.get_field(3), None);
+        assert_eq!(rec.get_field(4), None);
     }
 }
